fc2787aba78ee339b90017b630fe71d6fdbdf647
Luke Wahlmeier <lwahlmeier@gmail.com>
2019-09-23 09:46:24 -0600

    fixed run/dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
 FROM alpine:3.9
 
 RUN apk update && \
-    apk -Uuv add dumb-init ca-certificates && \
+    apk -Uuv add dumb-init ca-certificates bash && \
     rm /var/cache/apk/*
 COPY run.sh /run.sh
 RUN touch env.sh && chmod 755 /run.sh
 COPY build/stunning /stunning
 
+
+EXPOSE 80/tcp
+EXPOSE 3478/udp
+
+
 ENTRYPOINT ["/run.sh"]
-CMD ["/fs_exporter"]
+CMD ["/stunning"]
 
--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
-#!/usr/bin/dumb-init /bin/sh
+#!/usr/bin/dumb-init /bin/bash
+
+DEFAULT_STUNNING_LOGLEVEL="info"
+DEFAULT_STUNNING_STUNADDRESS="0.0.0.0:3478"
+DEFAULT_STUNNING_METRICSADDRESS="0.0.0.0:80"
+DEFAULT_STUNNING_POOLSIZE="25"
+DEFAULT_STUNNING_LOGGEOIP="false"
 
 . /env.sh
+
+for var in ${!DEFAULT_STUNNING*}; do
+  t=${var/DEFAULT_/}
+  if [ -z ${!t} ]; then
+    echo "Using default for ${t}:${!var}"
+    eval ${t}=${!var}
+    export "${t}"
+  else
+    echo "Using override value for ${t}"
+  fi
+done
+
 
 exec "${@}"
 
